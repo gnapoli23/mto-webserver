2d09c79583be013e313855218241af3875d5b804
Giovanni Napoli <gnapoli.dev@gmail.com>
2023-12-01 13:42:16 +0100

    fix basic auth

--- service/src/auth/basic.rs
+++ service/src/auth/basic.rs
@@ -9,22 +9,28 @@ pub async fn basic_auth(
     req: ServiceRequest,
     credentials: BasicAuth,
 ) -> Result<ServiceRequest, (Error, ServiceRequest)> {
-    let db = req.app_data::<web::Data<DatabaseConnection>>().unwrap();
+    if let Some(db) = req.app_data::<web::Data<DatabaseConnection>>() {
+        // Find user
+        let user = User::find()
+            .filter(UserColumn::Username.eq(credentials.user_id()))
+            .one(db.as_ref())
+            .await
+            .unwrap();
 
-    // Find user
-    let user = User::find()
-        .filter(UserColumn::Username.eq(credentials.user_id()))
-        .one(db.as_ref())
-        .await
-        .unwrap();
-
-    if let Some(user) = user {
-        if user.password.as_deref() == credentials.password() {
-            Ok(req)
+        // Check credentials
+        if let Some(user) = user {
+            if user.password.as_deref() == credentials.password() {
+                Ok(req)
+            } else {
+                Err((ServiceError::Unauthorized.into(), req))
+            }
         } else {
-            Err((ServiceError::Unauthorized.into(), req))
+            Err((ServiceError::NotFound.into(), req))
         }
     } else {
-        Err((ServiceError::NotFound.into(), req))
+        Err((
+            ServiceError::Crud(sea_orm::DbErr::Custom("Unable to get db connection".into())).into(),
+            req,
+        ))
     }
 }
